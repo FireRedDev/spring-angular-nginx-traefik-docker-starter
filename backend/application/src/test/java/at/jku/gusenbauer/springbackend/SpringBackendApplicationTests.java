c3a0aeef1afc769397e5454627d2f58fc9f2897b
chrisi.gusenbauer101@gmail.com <chrisi.gusenbauer101@gmail.com>
2021-10-08 15:53:48 +0200

    commit

---
+++ backend/application/src/test/java/at/jku/gusenbauer/springbackend/SpringBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package at.jku.gusenbauer.springbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
